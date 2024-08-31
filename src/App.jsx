ed377c41a47ede2640d13e49ea9a9b0de6adbf70
Mina Mamdouh <menamamdouh852456@gmail.com>
2024-08-31 14:17:30 +0300

    make routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-// import Login from './pages/Login';
-// import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import { Toaster } from 'react-hot-toast';
+import SignUp from './pages/SignUp';
+import { Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
+import { useSelector } from 'react-redux';
 function App() {
+  const { isLogin } = useSelector((store) => store.main);
   return (
     <div className='p-4 h-screen flex justify-center items-center'>
-      {/* <Login /> */}
-      {/* <SignUp /> */}
-      <Home />
+      <Routes>
+        <Route path='/' element={isLogin ? <Home /> : <Navigate to={'/login'} />} />
+        <Route
+          path='/login'
+          element={isLogin ? <Navigate to={'/'} /> : <Login />}
+        />
+        <Route
+          path='/signup'
+          element={isLogin ? <Navigate to={'/'} /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster position='top-center' reverseOrder={false} />
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,16 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Provider } from 'react-redux';
 import { store } from './Store/store.js';
+import { BrowserRouter } from 'react-router-dom';
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
         <ReactQueryDevtools />
       </QueryClientProvider>
     </Provider>
